954f718c69ed7be0324941169c408c2b94c4dbe2
Giovani Lima <giovanilima775@hotmail.com>
2021-12-11 20:10:19 -0300

    feat: GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/globals";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
